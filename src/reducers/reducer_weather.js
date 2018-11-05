a365b1ce861f412ae2240197ec8835aeb906ad4a
samy884 <ssgill884@gmail.com>
2018-11-05 19:49:47 +0530

    modified

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,10 @@
 import {FETCH_WEATHER} from '../actions/index';
 import {DELETECITY} from '../actions/deleterow';
 export default function(state=[],action){
-    console.log("state:");
+    /*console.log("state:");
     console.log(state);
     console.log("action:");
-    console.log(action);
+    console.log(action);*/
     switch(action.type){
         case FETCH_WEATHER:
             console.log([action.payload.data, ...state]);

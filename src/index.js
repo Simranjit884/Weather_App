7d8c2ed8b81a226a36bbc7cfe437c614bf4dae0a
samy884 <ssgill884@gmail.com>
2018-11-05 19:42:21 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
-import {loadState,saveState} from './localStorage';
+/*import {loadState,saveState} from './localStorage';
 
 const persistedState=loadState();
 const store=createStore(persistedState);
 
 store.subscribe(()=>{
   store.getState()
-});
+});*/
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
